e00dc3e8664e0f249e6f77f16c9e8bbe8c0a4185
Dhairya Anand <dhairya0431.be21@chitkara.edu.in>
2023-10-21 11:58:35 +0530

    Update Files

---
+++ client/Authentication/script.js
@@ -0,0 +1,94 @@
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+const errorMessage = document.getElementById("error-message");
+
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+const passwordRegex = /^(?=\S{9,}$)/;
+
+signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+	errorMessage.style.display = "none";
+
+	if (!email.match(emailRegex)) {
+        errorMessage.textContent = "Invalid email format";
+        errorMessage.style.display = "block";
+        return;
+    }
+
+    if(!password.match(passwordRegex)){
+        errorMessage.textContent = "Invalid password format.It should contain more than 8 characters";
+        errorMessage.style.display = "block";
+        return;
+    }
+
+    fetch("http://localhost:3000/auth/signup", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+    .then((data) => {
+        console.log(data.status);
+        if(data.status === 201) {
+            window.location.href = "./Login.html";
+        }
+        else if (data.status === 400) {
+            errorMessage.textContent = "Email already registered"; 
+            errorMessage.style.display = "block";
+        }
+    })
+});
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+	errorMessage.style.display = "none";
+
+	if (!email.match(emailRegex)) {
+        errorMessage.textContent = "Invalid email format";
+        errorMessage.style.display = "block";
+        return; 
+    }
+
+    if(!password.match(passwordRegex)){
+        errorMessage.textContent = "Invalid password format.It should contain more than 8 characters";
+        errorMessage.style.display = "block";
+        return;
+    }
+
+    fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+    })
+    .then((data) => {
+        console.log(data.status);
+        if(data.status === 201) {
+            window.location.href = "http://127.0.0.1:5500/client/Employee-Management/index.html"
+        }
+        else if(data.status === 403){
+            errorMessage.textContent = "Incorrect Password";
+            errorMessage.style.display = "block";
+        } 
+        else if(data.status === 404){
+            errorMessage.textContent = "User is not registered";
+            errorMessage.style.display = "block";
+        }
+    })
+});
